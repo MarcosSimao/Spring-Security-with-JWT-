403aad1c95297139865b9dacecbe92831a06ed70
MArcos Andrade <marcosjuremao@hotmail.com>
2021-11-21 22:41:51 -0300

    criando o crud repository, controller, service

--- useservice/src/main/java/io/gatarrays/useservice/service/UserServiceImpl.java
+++ useservice/src/main/java/io/gatarrays/useservice/service/UserServiceImpl.java
@@ -19,29 +19,34 @@ public class UserServiceImpl implements UserService{
     private final RoleRepository roleRepository;
     @Override
     public User saveUser(User user) {
+        log.info("salvando novos usuarios {} no banco de dados",user.getName());
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("salvando novos cargos {} no banco de dados",role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
-      User user= userRepository.findByUserName(username);
+      log.info("adicionando cargos {} no usuario {}",roleName,username);
+        User user= userRepository.findByUserName(username);
       Role role= roleRepository.findByName(roleName);
       user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
+        log.info("buscando usuario por username {}",username);
         User user=userRepository.findByUserName(username);
         return user;
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("listando todos os usuarios");
         return userRepository.findAll();
     }
 }
